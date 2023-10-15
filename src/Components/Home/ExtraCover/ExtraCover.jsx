563fa2e9be942602634b15ef9809b95eeaf7a892
kazibilsi <kazibilasi@gmail.com>
2023-10-15 20:46:07 +0600

    modify something

--- src/Components/Home/ExtraCover/ExtraCover.jsx
+++ src/Components/Home/ExtraCover/ExtraCover.jsx
@@ -9,7 +9,7 @@ const ExtraCover = () => {
             <div data-aos="zoom-in-up">
 
                 <div className=' absolute lg:text-2xl ml-[43%] text-white lg:mt-[10%] z-10 w-2/5 h-1/4 p-6 '>
-                    <p className='mb-3 lg:text-4xl text-xl font-extrabold text-gray-100'>Toys are all your baby need better toys for better growth.</p>
+                    <p className='mb-3 lg:text-4xl text-sm font-extrabold text-gray-100'>Toys are all your baby need better toys for better growth.</p>
                  
                     <p className='text-gray-100 lg:block md:block hidden '>
                         Learning toys often provide an open-ended play environment that encourages creativity and imagination. From art supplies that allow for self-expression to open-ended building sets that prompt imaginative constructions.
